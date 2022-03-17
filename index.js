8733e1e378d10b84ce1cc11c526fd08efa0b939e
Helder Santos <helder.santos@cazoo.co.uk>
2022-03-17 09:06:58 +0000

    Show PR Number & title

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const pull_request = github.context.payload.pull_request;
-  console.log(
-    `Pull request data: ${JSON.stringify(pull_request, undefined, 2)}`
-  );
+  // const pull_request = github.context.payload.pull_request;
+  const prNumber = github.context.payload.pull_request.number;
+  const title = github.context.payload.pull_request.title;
+  console.log(`Pull request data: PR-${prNumber}: ${title}`);
   console.log(`Event context: ${JSON.stringify(github.context, undefined, 2)}`);
 } catch (error) {
   core.setFailed(error.message);
